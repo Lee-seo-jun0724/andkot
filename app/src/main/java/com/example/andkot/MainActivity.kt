5d643b10c76ed783b0eb96592fd1ce734e547d19
Lee-seo-jun0724 <lyiman000@gmail.com>
2023-06-29 13:51:56 +0900

    My Word

--- app/src/main/java/com/example/andkot/MainActivity.kt
+++ app/src/main/java/com/example/andkot/MainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.andkot
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        val textView = findViewById<TextView>(R.id.textView)
-        textView.text = "Android Start"
+        setContent {
+            AndkotTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
     }
 }
